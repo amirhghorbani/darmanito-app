e533b187c316150965d036a8b873166a3a43e83b
amirhossein ghorbani <amirhghorbani@amirhosseins-MacBook-Pro.local>
2020-11-03 22:38:00 +0330

    add pages folder and and set main page as main landing page

---
+++ src/pages/MainLanding.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../components/Header/Header';
+import Sec1 from '../components/Sec1/Sec1';
+import Sec2 from '../components/Sec2/Sec2';
+import Sec3 from '../components/Sec3/Sec3';
+import Sec4 from '../components/Sec4/Sec4';
+import Sec5 from '../components/Sec5/Sec5';
+import Sec6 from '../components/Sec6/Sec6';
+import Sec7 from '../components/Sec7/Sec7';
+import Sec8 from '../components/Sec8/Sec8';
+
+function MainLanding() {
+    return (
+        <div className='container' style={{overflow: "hidden"}}>
+             <Header />
+             <Sec1 />
+             <Sec2 />
+             <Sec3 />
+             <Sec4 />
+             <Sec5 />
+             <Sec6 />
+             <Sec7 />
+             <Sec8 />
+        </div>
+    )
+}
+
+export default MainLanding
