d6d61ce5a4ccc7f9f30b1c082367094bbf943e3e
amirhossein ghorbani <amirhghorbani@amirhosseins-MacBook-Pro.local>
2020-11-04 17:39:39 +0330

    fix form validation in main page

--- src/components/Sec5/Inputs/Input/Input.js
+++ src/components/Sec5/Inputs/Input/Input.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
  class Input extends Component {
+
+    state = {
+        input : '',
+        inputIsValid : null
+    }
+
+    changeHandler = (event) => {
+        this.setState({input : event.target.value});
+        if (this.state.input.length !== 10) {
+            this.setState({inputIsValid : false}) 
+        } else {
+            this.setState({inputIsValid : true})
+        }
+    }
      
     render() {
         return (
             <div className='input'>
-                <input type='text' placeholder='شماره همراه را وارد کنید'/>
+                <input type='text' 
+                 placeholder='شماره همراه را وارد کنید'
+                 onChange={this.changeHandler}
+                 className={this.state.inputIsValid !== null ? (this.state.inputIsValid ? 'success' : 'danger') : ''}
+                 />
                 <button>بفرست</button>
             </div>
         )
