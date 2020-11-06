863e3893847b4735f5da723fbb7b2310d2f05201
amirhossein ghorbani <amirhghorbani@amirhosseins-MacBook-Pro.local>
2020-11-06 12:37:08 +0330

    complete all input validations in main page

--- src/components/Sec5/Inputs/Input/Input.js
+++ src/components/Sec5/Inputs/Input/Input.js
@@ -1,48 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import DangerIcon from '../../../../assets/images/danger-icon.svg'
+import DangerIcon from "../../../../assets/images/danger-icon.svg";
 
- class Input extends Component {
+class Input extends Component {
+  state = {
+    input: "",
+    inputIsValid: null,
+  };
 
-    state = {
-        input : '',
-        inputIsValid : null
+  changeHandler = (event) => {
+    this.setState({ input: event.target.value });
+    if (this.state.input.length !== 10) {
+      this.setState({ inputIsValid: false });
+    } else {
+      this.setState({ inputIsValid: true });
     }
+  };
 
-    changeHandler = (event) => {
-        this.setState({input : event.target.value});
-        if (this.state.input.length !== 10) {
-            this.setState({inputIsValid : false}) 
-        } else {
-            this.setState({inputIsValid : true})
-        }
-    }
-     
-    render() {
+  render() {
+    const dangerText = (
+      <div style={{ display: "flex" }}>
+        <img src={DangerIcon} alt="danger" />
+        <p
+          style={{
+            color: "red",
+            fontSize: "12px",
+            fontWeight: "lighter",
+            marginRight: "13.5px",
+          }}
+        >
+          شماره همراه صحیح نیست
+        </p>
+      </div>
+    );
 
-        const dangerText = (
-            <div style={{display: "flex"}}>
-                <img src={DangerIcon} alt='danger' />
-                <p style={{color : 'red', fontSize : '12px', fontWeight : 'lighter', marginRight : '13.5px'}}>
-                شماره همراه صحیح  نیست
-                </p>
-            </div>
-        )
-
-        return (
-            <div>
-                <div className='input'>
-                    <input type='text' 
-                    placeholder='شماره همراه را وارد کنید'
-                    onChange={this.changeHandler}
-                    className={this.state.inputIsValid !== null ? (this.state.inputIsValid ? 'success' : 'danger') : ''}
-                    />
-                    <button>بفرست</button>
-                </div>
-                {this.state.inputIsValid !== null ? (!this.state.inputIsValid ? dangerText : '') : ''}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <div className="input">
+          <input
+            type="text"
+            placeholder="شماره همراه را وارد کنید"
+            onChange={this.changeHandler}
+            className={
+              this.state.inputIsValid !== null
+                ? this.state.inputIsValid
+                  ? "success"
+                  : "danger"
+                : ""
+            }
+          />
+          <button>بفرست</button>
+        </div>
+        {this.state.inputIsValid !== null
+          ? !this.state.inputIsValid
+            ? dangerText
+            : ""
+          : ""}
+      </div>
+    );
+  }
 }
 
-export default Input
+export default Input;
--- src/components/Sec6/Sec6Input/Sec6Input.js
+++ src/components/Sec6/Sec6Input/Sec6Input.js
@@ -1,16 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../../../sass/Sec5.scss'
+import DangerIcon from "../../../assets/images/danger-icon.svg";
+
+import "../../../sass/Sec5.scss";
 
 function Sec6Input() {
+  const [input, setInput] = useState("");
+  const [inputIsValid, setInputIsValid] = useState(null);
 
-    return (
-        <div className='sec__6__input'>
-            <input type='text' placeholder='شماره همراه را وارد کنید'/>
-            <button>ثبت نام سریع</button>
-        </div>
-        
-    )
+  const changeHandler = (event) => {
+    const inputVal = event.target.value;
+    setInput(inputVal);
+    console.log(inputVal);
+
+    if (input.length !== 10) {
+      setInputIsValid(false);
+    } else {
+      setInputIsValid(true);
+    }
+  };
+
+  const dangerText = (
+    <div style={{ display: "flex" }}>
+      <img src={DangerIcon} alt="danger" />
+      <p
+        style={{
+          color: "red",
+          fontSize: "12px",
+          fontWeight: "lighter",
+          marginRight: "13.5px",
+        }}
+      >
+        شماره همراه صحیح نیست
+      </p>
+    </div>
+  );
+
+  return (
+    <div className="sec__6__input">
+      <div className="input">
+        <input
+          type="text"
+          placeholder="شماره همراه را وارد کنید"
+          onChange={changeHandler}
+          value={input}
+          className={
+            inputIsValid !== null ? (inputIsValid ? "success" : "danger") : ""
+          }
+        />
+        <button>ثبت نام سریع</button>
+      </div>
+      {inputIsValid !== null ? (!inputIsValid ? dangerText : "") : ""}
+    </div>
+  );
 }
 
-export default Sec6Input
+export default Sec6Input;
