fcded36edf3484ba96b052fdcef6101a2e7061f4
amirhossein ghorbani <amirhghorbani@amirhosseins-MacBook-Pro.local>
2020-11-07 13:54:12 +0330

    fix modular scss for navbar in main page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import headerStyle from '../Header/Header.module.scss'
+import styles from "../Header/Header.module.scss";
 
 // import "../../sass/Header.scss";
 
@@ -8,11 +8,12 @@ import Logo from "../../assets/images/header-logo.svg";
 import DropDown from "../../assets/images/dropdown-icon.svg";
 import DropUp from "../../assets/images/dropup-icon.svg";
 
+/* dropdown menu jsx code */
 const dropDiv = (
-  <div className="header__dropmenu">
+  <div className={styles.dropmenu}>
     <button>دانلود مستقیم درمانیتو</button>
     <hr />
-    <div className="dropmenu__items">
+    <div className="dropItems">
       <h3>صفحه اصلی</h3>
       <ul>
         <li>
@@ -41,8 +42,8 @@ class Header extends Component {
     show: false,
   };
 
-  iconchangeHandler = (e) => {
-    e.preventDefault();
+  iconchangeHandler = (event) => {
+    event.preventDefault();
     this.setState({
       isClicked: !this.state.isClicked,
       show: !this.state.show,
@@ -51,26 +52,39 @@ class Header extends Component {
 
   render() {
     return (
-      <div className={headerStyle.header}>
-        <div className={headerStyle.header__logos}>
+      <div className={styles.header}>
+        <div className={styles.items}>
           <a href="#">
             <img src={Logo} alt="header" />
           </a>
-          <a className={headerStyle.dropIcon} onClick={this.iconchangeHandler} href="#">
+          <a
+            className={styles.dropIcon}
+            onClick={this.iconchangeHandler}
+            href="#"
+          >
             <img src={this.state.isClicked ? DropDown : DropUp} alt="header" />
           </a>
           <nav>
             <ul>
-              <a href=''><li>صفحه اصلی</li></a>
-              <a href=''><li>درباره ما</li></a>
-              <a href=''><li>تماس باما</li></a>
-              <a href=''><li>عضویت داروخانه ها</li></a>
+              <li>
+                <a href="">صفحه اصلی</a>
+              </li>
+              <li>
+                <a href="">درباره ما</a>
+              </li>
+              <li>
+                <a href="">تماس باما</a>
+              </li>
+              <li>
+                <a href="">عضویت داروخانه ها</a>
+              </li>
             </ul>
           </nav>
-         
-            <button className={headerStyle.download__btn}>دانلود درمانیتو</button>
-          
+
+          <button className={styles.download__btn}>دانلود درمانیتو</button>
         </div>
+
+        {/* dropdown menu in mobile devices only */}
         {this.state.show ? dropDiv : null}
       </div>
     );
